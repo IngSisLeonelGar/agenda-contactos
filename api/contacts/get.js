e0680bc5779a627e746ddd750fce5e47f2c5ff98
Leonel Garcia <ing.leonelgarciaflo@gmail.com>
2025-07-20 14:18:37 -0600

    prueba get

--- api/contacts/get.js
+++ api/contacts/get.js
@@ -1,20 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
-
-  const { data, error } = await supabase
-    .from('contacts')
-    .select('*')
-    .order('create_at', { ascending: false });
-
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-
-  res.status(200).json(data);
+  res.status(200).json({ message: 'GET funcionando!' });
 }
